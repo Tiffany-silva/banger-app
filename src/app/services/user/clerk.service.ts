7c44529778bafcdf952cd409862c202f80a0029e
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2020-12-10 00:28:35 +0530

    added clerk servce

--- src/app/services/user/clerk.service.ts
+++ src/app/services/user/clerk.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const baseUrl = 'http://localhost:8080/api/clerk/';
 @Injectable({
   providedIn: 'root'
 })
 export class ClerkService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postUpdateEmail(): Observable<any> {
+    return this.http.put(baseUrl + 'updateEmail', { responseType: 'text' });
+  }
+
+  postUpdatePassword(): Observable<any> {
+    return this.http.put(baseUrl + 'updatePassword', { responseType: 'text' });
+  }
+
+  postUpdateName(): Observable<any> {
+    return this.http.put(baseUrl + 'updateName', { responseType: 'text' });
+  }
+
+  getAllClerks(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  get(id:any): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+
+  create(data:any): Observable<any> {
+    return this.http.post(baseUrl,data);
+  }
+
+  delete(id:any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  // deleteAll(): Observable<any> {
+  //   return this.http.delete(baseUrl);
+  // }
+
 }
