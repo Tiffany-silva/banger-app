161a984ef99ec7a23ff1e1ce25e94105c2e0c0e9
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2020-12-10 03:38:41 +0530

    added services for additional equipment

--- src/app/services/additionalEquipment/additional-equipment.service.ts
+++ src/app/services/additionalEquipment/additional-equipment.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const baseUrl = 'http://localhost:8080/api/clerk/';
 @Injectable({
   providedIn: 'root'
 })
 export class AdditionalEquipmentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateQuantity(id:any, data:any): Observable<any> {
+    return this.http.put(`${baseUrl}/updateQuantity/${id}`, data);
+  }
+
+  findAllAvailable(id:any, data:any): Observable<any> {
+    return this.http.get(`${baseUrl}/findAllAvailable`, data);
+  }
+  
+  getAllAEquipments(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  getAEquipment(id:any): Observable<any> {
+    return this.http.get(`${baseUrl}/updateBookingStatus/${id}`);
+  }
+
+  createAEquipment(data:any): Observable<any> {
+    return this.http.post(baseUrl,data);
+  }
+
+  deleteAEquipment(id:any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAllAEquipments(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
 }
