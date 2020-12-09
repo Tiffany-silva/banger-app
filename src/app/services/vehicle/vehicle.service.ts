d34223a637afaeb7c20399afb50161fa784ed611
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2020-12-10 03:39:35 +0530

    added services for vehicle

--- src/app/services/vehicle/vehicle.service.ts
+++ src/app/services/vehicle/vehicle.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
+const baseUrl = 'http://localhost:8080/api/clerk/';
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  updateQuantity(id:any, data:any): Observable<any> {
+    return this.http.put(`${baseUrl}/updateQuantity/${id}`, data);
+  }
+
+  updatePrice(id:any, data:any): Observable<any> {
+    return this.http.put(`${baseUrl}/updatePrice/${id}`, data);
+  }
+  
+  getAllVehicles(): Observable<any> {
+    return this.http.get(baseUrl);
+  }
+
+  getAllBookingsofVehicle(data:any): Observable<any> {
+    return this.http.get(`${baseUrl}/findAllBookingsofVehicle`, data);
+  }
+
+  getVehicle(id:any): Observable<any> {
+    return this.http.get(`${baseUrl}/${id}`);
+  }
+  
+  createVehicle(data:any): Observable<any> {
+    return this.http.post(baseUrl,data);
+  }
+
+  deleteVehicle(id:any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+
+  deleteAllVehicles(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+
 }
