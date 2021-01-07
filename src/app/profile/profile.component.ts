97fc841d2ee90b85686434b2822ebab8ac8354b5
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2021-01-07 21:29:54 +0530

    profile component  added to web

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { formatISO, parse, parseISO } from 'date-fns';
+import { Observable } from 'rxjs';
+import { BookingService } from '../services/booking/booking.service';
+import { TokenStorageService } from '../services/token-storage.service';
+import { HirerService } from '../services/user/hirer.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +14,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  myProfile:any;
+  id:any;
+  myBookings:any=[];
+  products = [
+    { name: "Product A", description: "some description", picture: { url: 'https://dummyimage.com/600x150/000/fff' } },
+    { name: "Product B", description: "some description", picture: { url: 'https://dummyimage.com/300x300/000/fff' } },
+    { name: "Product C", description: "some description", picture: { url: 'https://dummyimage.com/300x400/000/fff' } },
+    { name: "Product C", description: "some description", picture: { url: 'https://dummyimage.com/300x400/000/fff' } },
+    { name: "Product C", description: "some description", picture: { url: 'https://dummyimage.com/300x400/000/fff' } },
+    { name: "Product D", description: "some description", picture: { url: 'https://dummyimage.com/600x500/000/fff' } }]
 
-  ngOnInit(): void {
+  addProduct() {
+    this.products.push({ name: Math.random().toString(36).substring(7), description: Math.random().toString(36).substring(50), picture: { url: 'https://dummyimage.com/600x400/000/fff' } })
+  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  obs: Observable<any>;
+  dataSource: MatTableDataSource<any>;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, private tokenService:TokenStorageService, 
+    private hirerService:HirerService, private bookingService:BookingService) {
+    this.getUserDetails();
+    console.log(this.obs);
+    
+  }
+  
+
+  ngOnInit() {
+    this.getUserBookings();
+    
   }
 
+  ngOnDestroy() {
+    if (this.dataSource) { 
+      this.dataSource.disconnect(); 
+    }
+  }
+
+  getUserDetails(){
+    this.id= JSON.parse(this.tokenService.getUser()).id;
+    this.hirerService.getHirer(this.id).subscribe(data=>{
+      this.myProfile=data;
+    })
+  }
+
+  getUserBookings(){
+    this.bookingService.findAllBookingsOfUser(this.id).subscribe(data=>{
+      data.forEach((element:any) => {
+        element.bookingDate= formatISO(parseISO(element.bookingDate), { representation: 'date' });
+        element.returnDate=  formatISO(parseISO(element.returnDate), { representation: 'date' });
+      });
+
+      this.myBookings=data;
+      this.dataSource= new MatTableDataSource<any>(this.myBookings);
+      this.changeDetectorRef.detectChanges();
+      this.dataSource.paginator = this.paginator;
+      this.obs = this.dataSource.connect();
+    })
+  }
 }
