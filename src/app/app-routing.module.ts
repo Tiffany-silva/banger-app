fe027c67a54b0b019077554569ab4b587f702806
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2021-01-07 21:19:28 +0530

    configuration and high level app  changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,67 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookingManagementDashboardComponent } from './clerk/booking-management-dashboard/booking-management-dashboard.component';
+import { ClerkHomeComponent } from './clerk/clerk-home/clerk-home.component';
+import { HomeComponent } from './home/home.component';
+import { ItemManagementDashboardComponent } from './clerk/item-management-dashboard/item-management-dashboard.component';
 import { LoginComponent } from './login/login.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full' },
-  {path: 'tutorials', component: LoginComponent}
+  {
+    path: '',
+    component: LoginComponent,
+},
+{
+    path: 'login',
+    component: LoginComponent,
+},
+{
+  path: 'signup',
+  component: SignupComponent,
+},
+{
+  path: 'home',
+  component: HomeComponent,
+},
+{
+  path: 'nav',
+  component: NavMenuComponent,
+},
+{
+  path: 'vehicle-detail/:id',
+  component: VehicleDetailComponent,
+},
+{
+  path: 'profile',
+  component: ProfileComponent,
+},
+{
+  path: 'profile-settings',
+  component: ProfileSettingsComponent,
+},
+{
+  path:'my-bookings',
+  component:MyBookingsComponent
+},
+{
+  path:'clerk-home',
+  component:ClerkHomeComponent
+},
+{
+  path:'clerk-item-management',
+  component:ItemManagementDashboardComponent
+},
+{
+  path:'clerk-booking-management',
+  component:BookingManagementDashboardComponent
+}
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
-import { ClerkHomeComponent } from './clerk-home/clerk-home.component';
+import { ClerkHomeComponent } from './clerk/clerk-home/clerk-home.component';
 import { VehicleDetailComponent } from './vehicle-detail/vehicle-detail.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 import { authInterceptorProviders } from './helpers/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './angular-material.module';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
+import { ExtendReturnDateDialogComponent } from '../app/dialogs/extend-return-date-dialog/extend-return-date-dialog.component';
+import { NgxMatDatetimePickerModule, NgxMatNativeDateModule, NgxMatTimepickerModule } from '@angular-material-components/datetime-picker';
+import { DeleteDialogComponent } from './dialogs/user-clerk/delete-dialog/delete-dialog.component';
+import { EditDialogComponent } from './dialogs/user-clerk/edit-dialog/edit-dialog.component';
+import { AddDialogComponent } from './dialogs/user-clerk/add-dialog/add-dialog.component';
+import { NavClerkMenuComponent } from './nav-clerk-menu/nav-clerk-menu.component';
+import { ItemManagementDashboardComponent } from './clerk/item-management-dashboard/item-management-dashboard.component';
+import { AddItemDialogComponent } from './dialogs/add-item-dialog/add-item-dialog.component';
+import { EditItemDialogComponent } from './dialogs/edit-item-dialog/edit-item-dialog.component';
+import { DeleteItemDialogComponent } from './dialogs/delete-item-dialog/delete-item-dialog.component';
+import { BookingManagementDashboardComponent } from './clerk/booking-management-dashboard/booking-management-dashboard.component';
 
 @NgModule({
   declarations: [
@@ -24,12 +41,35 @@ import { authInterceptorProviders } from './helpers/auth.interceptor';
     VehicleDetailComponent,
     ProfileComponent,
     ProfileSettingsComponent,
+    NavMenuComponent,
+
+    MyBookingsComponent,
+    ExtendReturnDateDialogComponent,
+    AddDialogComponent,
+    DeleteDialogComponent,
+    EditDialogComponent,
+    NavClerkMenuComponent,
+    ItemManagementDashboardComponent,
+    AddItemDialogComponent,
+    EditItemDialogComponent,
+    DeleteItemDialogComponent,
+    BookingManagementDashboardComponent
   ],
+  entryComponents:[ExtendReturnDateDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    NgxMatDatetimePickerModule,
+    NgxMatNativeDateModule,
+    NgxMatTimepickerModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+    LayoutModule,
+    FlexLayoutModule
+
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
