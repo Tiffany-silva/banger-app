69afaa385f6332d6b55f3c9b76da791f73fe358a
Supeshala Silva <dilanangammana@Dilans-MacBook-Pro.local>
2021-01-07 21:23:35 +0530

    add item dialog component added to web

---
+++ src/app/dialogs/add-item-dialog/add-item-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { VehicleType } from '../../entity.Models/vehicle';
+
+@Component({
+  selector: 'app-add-item-dialog',
+  templateUrl: './add-item-dialog.component.html',
+  styleUrls: ['./add-item-dialog.component.scss']
+})
+export class AddItemDialogComponent implements OnInit {
+
+  vehicleType:VehicleType[]=[VehicleType.DIESEL_AUTO, VehicleType.HYBRID, VehicleType.PETROL, VehicleType.PETROL_MANUAL];
+
+
+  constructor(public dialogRef: MatDialogRef<AddItemDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+    
+  ngOnInit(): void {
+  }
+  
+
+  formControl = new FormControl('', [
+    Validators.required
+    // Validators.email,
+  ]);
+
+  getErrorMessage() {
+    return this.formControl.hasError('required') ? 'Required field' :
+      this.formControl.hasError('email') ? 'Not a valid email' :
+        '';
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
