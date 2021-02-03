adbb8ad42a0a7d1f207a908618f0d7188978da4d
Tiffany <supeshalapragathisilva@gmail.com>
2021-02-03 16:09:18 +0530

    added auth and role guards

--- src/app/helpers/auth.interceptor.ts
+++ src/app/helpers/auth.interceptor.ts
@@ -13,11 +13,15 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    const token = this.token.getToken();
-    if (token != null) {
+    if(req.url.indexOf('https://api.imgbb.com/1/upload') !== -1) {
+      return next.handle(req);
+    } else{
+      const token = this.token.getToken();
+      if (token != null) {
         authReq = req.clone({ headers: req.headers.set(TOKEN_HEADER_KEY, token) });
       }
-      return next.handle(authReq)
+    }
+    return next.handle(authReq);
   }
 }
 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {HttpClient, HttpEvent, HttpHandler, HttpHeaders, HttpRequest} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Clerk } from '../entity.Models/clerk';
 import { Hirer } from '../entity.Models/hirer';
+import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 const AUTH_API = 'http://localhost:8080/api/auth/';
 
@@ -14,18 +16,19 @@ const httpOptions = {
 })
 
 export class AuthService {
-  
-    constructor(private http: HttpClient) { }
-  
-    login(credentials: { email: any; password: any;}): Observable<any> {
+    private readonly apiKey:string='659ad79210abf40244561adc10b63988';
+    constructor(private http: HttpClient) {
+
+    }
+
+    login(credentials: { email: any; password: any }): Observable<any> {
       return this.http.post(AUTH_API + 'login', {
         email: credentials.email,
         password: credentials.password,
       }, httpOptions);
     }
-  
+
     registerClerk(user: Clerk): Observable<any> {
-      console.log("1111111")
       console.log(user);
 
       return this.http.post(AUTH_API + 'signup', {
@@ -36,28 +39,50 @@ export class AuthService {
       }, httpOptions);
     }
 
-    registerHirer(user: Hirer): Observable<any> {
-      console.log('-------');
-      console.log(user);
-      return this.http.post(AUTH_API + 'signup', {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        dob: user.dob,
-        photoURL: user.photoURL,
-        confirmIdentity: user.confirmIdentity,
-        drivingLicenseUrl: user.drivingLicenseURL,
-        address: user.address,
-        email:user.email,
-        password: user.password,
-        blackListed: user.blackListed,
-        role:user.role
-      }, httpOptions);
-    }
+    registerHirer(user: Hirer, url:string) {
+        return this.http.post(AUTH_API + 'signup', {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          nic: user.nic,
+          dob: user.dob,
+          photoURL: url,
+          confirmIdentity: user.confirmIdentity,
+          drivingLicenseUrl: user.drivingLicenseURL,
+          address: user.address,
+          email:user.email,
+          password: user.password,
+          blackListed: user.blackListed,
+          role:user.role
+        }, httpOptions);
+      };
+
+      // const userBlob = new Blob([myObjStr], { type: 'application/json'});
+      // console.log(userBlob);
+      // formdata.append('user', userBlob);
+      // console.log(formdata);
+      // return this.http.post(AUTH_API + 'signup', formdata, httpOptions);
+
+
 
     logout(role:any, id:any):Observable<any>{
       console.log("im hereeee")
       console.log(role)
       return this.http.post(AUTH_API+"logout/"+id,role);
     }
+
+    uploadImageAndGetURL(file:any): Observable<any>{
+      let formdata: FormData = new FormData();
+      formdata.append('image', file[0]);
+
+      return this.http.post(`https://api.imgbb.com/1/upload`, formdata, {params :{key: this.apiKey}})
+        .pipe(map((response:any)=>response['data']['url']));
+    }
+
+  // public isAuthenticated(): boolean {
+  //   const token = localStorage.getItem('token');
+  //   // Check whether the token is expired and return
+  //   // true or false
+  //   return !this.jwtHelper.isTokenExpired(token);
+  // }
 
 }
